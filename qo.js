816952042b492c86526f849367d9f774efa048ef
ahcgnoej <jinjinchachajung@naver.com>
2024-07-25 17:16:06 +0900

    0725

--- qo.js
+++
@@ -1,9 +0,0 @@
-const todos=['1','2','3','4'];
-todos.push('5');
-let a= todos.pop()
-todos.push(a)
-todos[10]='10'
-todos.splice(5,5)
-console.log(todos.length)
-console.log(todos)
-console.log(todos.indexOf('3'))
